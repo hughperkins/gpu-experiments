877e9ebba7045d66f66b144d0a8a38450b32ea9c
Hugh Perkins <hughperkins@gmail.com>
2016-07-10 01:48:05 +0800

    factorize generic functions out of volkov1.py

--- gpuexperiments/volkov1.py
+++ gpuexperiments/volkov1.py
@@ -13,89 +13,10 @@ from os.path import join
 from gpuexperiments.callkernel import call_cl_kernel
 #import gpuexperiments.cpu_check
 from gpuexperiments.timecheck import inittime, timecheck
+from lib_clgpuexp import clearComputeCache, getPtx, timeKernel, buildKernel, initClGpu
 
-gpu_idx = 0
 
-platforms = cl.get_platforms()
-i = 0
-for platform in platforms:
-   gpu_devices = platform.get_devices(device_type=cl.device_type.GPU)
-   if gpu_idx < i + len(gpu_devices):
-       ctx = cl.Context(devices=[gpu_devices[gpu_idx-i]])
-       break
-   i += len(gpu_devices)
-
-print('context', ctx)
-q = cl.CommandQueue(ctx)  
-
-mf = cl.mem_flags
-
-def clearComputeCache():
-    cache_dir = join(os.environ['HOME'], '.nv/ComputeCache')
-    for subdir in os.listdir(cache_dir):
-        if subdir == 'index':
-            continue
-        print('clean', subdir)
-        subprocess.call(['rm', '-Rf', join(cache_dir, subdir)])
-#    subprocess.call(['rm', '-Rf', join(os.environ['HOME'], '.nv/ComputeCache')])
-
-def getPtx(kernelName):
-    with open('/tmp/gpucmd.sh', 'w') as f:
-        f.write(r"""#!/bin/bash
-        cat $(grep -r %s ~/.nv/ComputeCache | awk '{print $3}')
-"""  % kernelName)
-    filepath = subprocess.check_output(['/bin/bash', '/tmp/gpucmd.sh'])
-    filepath_utf8 = ''
-    for byte in filepath:
-        # print(byte)
-        if byte >= 10 and byte < 128:
-           if chr(byte) in string.printable:
-               filepath_utf8 += chr(byte)
-    # print('filepath', filepath)
-    #print(kernelName)
-    ptx = filepath_utf8.split('--opt-level')[0]
-    print(ptx)
-    return ptx
-
-def dumpSass(kernelName):
-    ptx = getPtx(kernelName)
-    ptx = ptx.split('.version 5.0')[1].split('A')[0]
-    ptx = '.version 4.3\n' + ptx
-    # print('ptx', ptx)
-    #sys.exit(1)
-    with open('/tmp/~kernel.ptx', 'w') as f:
-        f.write(ptx)
-    print(subprocess.check_output([
-        'ptxas',
-        '--gpu-name', 'sm_50',
-        '--output-file', '/tmp/~kernel.o',
-        '/tmp/~kernel.ptx']).decode('utf-8'))
-    sass = subprocess.check_output([
-        'cuobjdump', '--dump-sass', '/tmp/~kernel.o']).decode('utf-8')
-    print(sass)
-    return sass
-
-def buildKernel(name, source, options=''):
-    # options = '-cl-opt-disable'
-    # options = ''
-    return cl.Program(ctx, source).build(options=options).__getattr__(name) 
-
-d = np.zeros((1024*1024 * 32 * 2,), dtype=np.float32)
-d_cl = cl.Buffer(ctx, mf.READ_WRITE | mf.COPY_HOST_PTR, hostbuf=d)
-
-out = np.zeros((1024,), dtype=np.float32)
-out_cl = cl.Buffer(ctx, mf.READ_WRITE | mf.COPY_HOST_PTR, hostbuf=out)
-
-def timeKernel(name, kernel, grid_x=1, block_x=32):
-    # clearComputeCache()
-    grid = (grid_x,1,1)
-    block = (block_x,1,1)
-    q.finish()
-    inittime()
-    call_cl_kernel(kernel, q, grid, block, d_cl, out_cl)
-    q.finish()
-    return timecheck(name)
-    # print(getPtx('mykernel'))
+initClGpu()
 
 times = []
 
@@ -172,7 +93,7 @@ for experiment in experiments:
         times.append({'name': name, 'time': t, 'flops': flops})
 
 
-print('kernel time(ms) GFLOPS')
+print('name\t\t\ttot ms\tgflops')
 for time_info in times:
-    print('%s %s %s' % (time_info['name'], time_info['time'], time_info.get('flops', '') / 1000 / 1000 / 1000))
+    print('%s\t%.1f\t%.0f' % (time_info['name'].ljust(23), time_info['time'], time_info.get('flops', '') / 1000 / 1000 / 1000))
 
