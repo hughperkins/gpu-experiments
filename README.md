87f289e85b408593f7ec0ec3fb588ea7c00c009a
Hugh Perkins <hughperkins@gmail.com>
2016-07-13 11:37:23 +0800

    show effect of gridsize on outgoing bandwidth

--- README.md
+++ README.md
@@ -176,6 +176,16 @@ Let's do some other simple tests first though.  Here is effect of blocksize on b
 
 We can see that bandwidth is linear in blocksize, until we saturate the available bandwidth, at a blocksize of ~128 threads.  Note that it looks like we can get full bandwidth using a single block, ie *running on a single compute unit*.
 
+Let's try varying similarly the gridsize, with a single block of 32 threads.
+
+[gpuexperiments/globalwrite_gridsize.py](gpuexperiments/globalwrite_gridsize.py)
+
+<img src="img/globalwrite_gridsize_940m.png?raw=true" width="600" height="400" />
+
+[results/globalwrite_gridsize_940m.tsv](results/globalwrite_gridsize_940m.tsv)
+
+A grid of 32 blocks of 32 threads is sufficient to saturate the bandwidth, on a 940M.  Actually, just 8 blocks of 32 threads reaches 12GiB/seconds, within about ~20% of full bandwidth.
+
 ## other things to check maybe
 
 * Effect of barrier on performance
---
+++ gpuexperiments/globalwrite_gridsize_graphs.py
@@ -0,0 +1,77 @@
+from __future__ import print_function, division
+import argparse
+import string
+import numpy as np
+import os
+from collections import defaultdict
+import array
+import csv
+import matplotlib.pyplot as plt
+plt.rcdefaults()
+import matplotlib.pyplot as plt
+from os.path import join
+import lib_clgpuexp
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--devicename', required=True)
+args = parser.parse_args()
+
+assert args.devicename is not None
+
+times = []
+
+deviceNameSimple = args.devicename
+f = open('results/globalwrite_gridsize_%s.tsv' % args.devicename, 'r')
+reader = csv.DictReader(f, delimiter='\t')
+for row in reader:
+    times.append({'name': row['name'], 'bw': float(row['bw gib'])})
+f.close()
+
+X_lists = []
+Y_lists = []
+names = []
+
+def getNameValue(name, key, default=None):
+    for bit in name.split('_'):
+        if bit.startswith(key):
+            return int(bit.replace(key, ''))
+    if default is not None:
+        return default
+    raise Exception('key %s not found in %s' % (key, name))
+
+lastFamily = None
+list_idx = -1
+for timeinfo in times:
+    name = timeinfo['name']
+    family = 'a'
+    blockSize = int(name.replace('gridsize', ''))
+    if family != lastFamily:
+        list_idx += 1
+        X_lists.append([])
+        Y_lists.append([])
+        lastFamily = family
+        names.append('gridsize 1')
+    x = blockSize
+    y = timeinfo['bw']
+    X_lists[list_idx].append(x)
+    Y_lists[list_idx].append(y)
+
+thismax = 0
+maxx = 0
+for i, name in enumerate(names):
+    X = np.array(X_lists[i])
+    Y = np.array(Y_lists[i])
+    plt.plot(X, Y, '-x', label=name)
+    thismax = max(thismax, max(Y))
+    maxx = max(maxx, max(X))
+maxx = 48
+
+#plt.axis([0, max(X), 0, max(Y)])
+plt.axis([0, maxx, 0, thismax])
+plt.title(deviceNameSimple)
+plt.xlabel('Gridsize')
+plt.ylabel('Bandwidth (GiB/s)')
+# legend = plt.legend(loc='lower right') # fontsize='x-large')
+plt.savefig('/tmp/globalwrite_gridsize_%s.png' % deviceNameSimple, dpi=150)
+
